c4e61d7cbaf6455ec1966df3f8c4643035f559dc
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-04-28 15:13:23 -0700

    finished fixing refresh error with rendering each card on BoardGameList component.

--- src/components/BoardgameList/BoardgameList.jsx
+++ src/components/BoardgameList/BoardgameList.jsx
@@ -1,7 +1,7 @@
 import { RDS } from 'aws-sdk';
 import { set } from 'mongoose';
-import React, {useState, useEffect} from 'react';
 import { Card } from 'semantic-ui-react';
+import React, {useState, useEffect} from 'react';
 import BoardgameCard from '../BoardgameCard/BoardgameCard';
 
 export default function BoardgameList({ atlasApiUrl, bgData, handleAddFav }){
@@ -9,7 +9,7 @@ export default function BoardgameList({ atlasApiUrl, bgData, handleAddFav }){
     const [boardgames, setBoardgames] = useState([])
 
     useEffect(() => {
-        if(bgData === undefined){
+        if(bgData === undefined || boardgames.length === 0){
             fetch(atlasApiUrl)
             .then((res) => res.json())
             .then((data) => {
@@ -20,29 +20,35 @@ export default function BoardgameList({ atlasApiUrl, bgData, handleAddFav }){
 
     let cards = boardgames.map(boardgame => {
         return (
-            <BoardgameCard
-                name={boardgame.name}
-                image={boardgame.image_url}
-                description={boardgame.description_preview}
-                minPlayers={boardgame.min_players}
-                maxPlayers={boardgame.max_players}
-                minPlaytime={boardgame.min_playtime}
-                maxPlaytime={boardgame.max_playtime}
-                minAge={boardgame.min_age}
-                publishers={boardgame.primary_publisher.name}
-                release={boardgame.year_published}
-                rating={boardgame.average_user_rating}
-                id={boardgame.id}
-                key={`${boardgame.name}`}
-                handleAddFav={handleAddFav}
-            />
+                <BoardgameCard
+                    name={boardgame.name}
+                    image={boardgame.image_url}
+                    description={boardgame.description_preview}
+                    minPlayers={boardgame.min_players}
+                    maxPlayers={boardgame.max_players}
+                    minPlaytime={boardgame.min_playtime}
+                    maxPlaytime={boardgame.max_playtime}
+                    minAge={boardgame.min_age}
+                    publishers={boardgame.primary_publisher.name}
+                    release={boardgame.year_published}
+                    rating={boardgame.average_user_rating}
+                    id={boardgame.id}
+                    key={`${boardgame.name}`}
+                    handleAddFav={handleAddFav}
+                />
         )
     })
 
     console.log(cards, 'THESE ARE THE CARDS')
 
     return(
-        <h3>{boardgames ? cards : 'Loading...'}</h3>
+        <>
+        {boardgames ?
+        <Card.Group itemsPerRow={4} stackable>
+            {cards}
+        </Card.Group>
+        : <h3>Loading...</h3>}
+        </>
     )
 
 
