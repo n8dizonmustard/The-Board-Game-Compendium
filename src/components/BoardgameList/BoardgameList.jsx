2599982e656325d730bc4827edb32135d9776c26
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-05-01 15:44:00 -0700

    refactor boardgamelist component to have loading appear on both profilepage and boardgamespage.

--- src/components/BoardgameList/BoardgameList.jsx
+++ src/components/BoardgameList/BoardgameList.jsx
@@ -21,39 +21,22 @@ export default function BoardgameList({ atlasApiUrl, bgData, handleFavorite, use
         })
     }, [])
 
-
-
     return (
-        <>
-        {// PART 1
-        boardgames.length < 1 && userFavorites.length < 1 ? // ARE GAMES LOADING?
+        <>{
+
+        // PART 1: if userFavs are empty, render loading
+        userFavorites.length < 1 ?
             <>
             <h1>LOADING BOARD GAMES...</h1>
             <Image size='small' src='https://icon-library.com/images/hammer-icon-png/hammer-icon-png-18.jpg' />
             </>
 
         :
-            // PART 2
-            isProfile ?
-            <Card.Group itemsPerRow={3} stackable>
-                {userFavorites.map(boardgame => 
-                    <BoardgameCard
-                        name={boardgame.name}
-                        image={boardgame.image_url}
-                        id={boardgame.id}
-                        key={`${boardgame.name}`}
-                        handleFavorite={handleFavorite}
-                        user={user}
-                        userFavorites={userFavorites}
-                        boardgame={boardgame}
-                    />)
-                }
-            </Card.Group>
-
-        :
-            // PART 3
-            <Card.Group itemsPerRow={5} stackable>
-            {boardgames.map(boardgame => 
+        // PART 2: if userFavs are NOT empty, check if page isProfile
+        //      if isProfile == true, render userFavs
+        isProfile ?
+        <Card.Group itemsPerRow={3} stackable>
+            {userFavorites.map(boardgame => 
                 <BoardgameCard
                     name={boardgame.name}
                     image={boardgame.image_url}
@@ -65,9 +48,33 @@ export default function BoardgameList({ atlasApiUrl, bgData, handleFavorite, use
                     boardgame={boardgame}
                 />)
             }
-            </Card.Group>
+        </Card.Group>
+
+        :
+        // PART 3: if page isProfile == false, check if boardgames is empty
+        boardgames.length < 1 ?
+            <>
+            <h1>LOADING BOARD GAMES...</h1>
+            <Image size='small' src='https://icon-library.com/images/hammer-icon-png/hammer-icon-png-18.jpg' />
+            </>
+
+        :
+        // PART 4: if boardgames is NOT empty, render boardgames
+        <Card.Group itemsPerRow={5} stackable>
+        {boardgames.map(boardgame => 
+            <BoardgameCard
+                name={boardgame.name}
+                image={boardgame.image_url}
+                id={boardgame.id}
+                key={`${boardgame.name}`}
+                handleFavorite={handleFavorite}
+                user={user}
+                userFavorites={userFavorites}
+                boardgame={boardgame}
+            />)
         }
-        </>
+        </Card.Group>
+        }</>
     )
 
 }
