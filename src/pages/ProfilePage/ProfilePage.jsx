0ea74931527f8d88f6f4f67a309e37a0ce5d17a4
Nathaniel Dizon <n8dizon@nathaniels-air.lan>
2021-05-02 16:53:03 -0700

    finished styling profile page.

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import './ProfilePage.css';
+import { Grid, Image } from 'semantic-ui-react';
 import PageHeader from '../../components/Header/Header';
 import BoardgameList from '../../components/BoardgameList/BoardgameList';
 
@@ -7,19 +8,30 @@ export default function ProfilePage({ user, userFavorites, handleFavorite }){
 
     // console.log('this is user info', user)
 
-    const style = {
-        color: 'white'
+    const black = {
+        color: 'black'
     }
 
     return (
         <>
         <PageHeader user={user}/>
-        <Image src={user.photoUrl} size='medium' circular floated='right'/>
-        <h1>{user.username}'s Profile</h1>
-        <h4 style={style}>Email: {user.email}</h4>
-        <h4 style={style}>About me: {user.bio}</h4>
-        <h2 style={style}>My Favorite Games:</h2> 
-        <BoardgameList userFavorites={userFavorites} isProfile={true} handleFavorite={handleFavorite}/>
+        <Grid>
+            <Grid.Row>
+                <Grid.Column width={3} className='user-column'>
+                    <Image src={user.photoUrl} size='medium' circular className='profile-pic'/>
+                    <div className='user-info'>
+                        <h1 style={black}>{user.username}'s Profile</h1>
+                        <h4>Email: {user.email}</h4>
+                        <h4>About me: {user.bio}</h4>
+                    </div>
+                </Grid.Column>
+
+                <Grid.Column width={13}>
+                    <h2 className='favorite-header'>My Favorite Games:</h2> 
+                    <BoardgameList userFavorites={userFavorites} isProfile={true} handleFavorite={handleFavorite}/>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
         </>
     )
 }
